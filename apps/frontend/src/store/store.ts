7f069cde6033082285c9674fe2f164ee3883f147
Faizan <syedfaizan.edu@gmail.com>
2025-01-17 18:50:31 +0530

    first commit

---
+++ apps/frontend/src/store/store.ts
@@ -0,0 +1,16 @@
+import { configureStore } from '@reduxjs/toolkit';
+// Import slices
+import exampleReducer from './exampleSlice';
+
+// Create the store
+export const store = configureStore({
+    reducer: {
+        example: exampleReducer
+    },
+});
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export default store;
